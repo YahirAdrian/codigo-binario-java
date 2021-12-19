7919aaf2dd431afee8f91eb83341e26989705b8d
Yahir Adrian <leonyahir4686@gmail.com>
2021-12-18 21:41:55 -0500

    Aplicación funcional

--- src/app/CambiarDecimal.java
+++
@@ -1,6 +0,0 @@
-package app;
-
-public  class CambiarDecimal {
-	int decimal = 0, ultimo_boton = 0, suma=0;
-	
-}
--- src/app/Contenido.java
+++ src/app/Contenido.java
@@ -1,22 +1,35 @@
 package app;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*; 
 
-public class Contenido extends JPanel{
+public class Contenido extends JPanel implements ActionListener{
 	JButton boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8;
 	JButton botones [] = {boton1, boton2, boton3, boton4, boton5, boton6, boton7, boton8};
 	JLabel decimalLabel = new JLabel("Decimal = 0");
 	
+	ImageIcon focoApagado = new ImageIcon("src/recursos/apagado.png");
+	Icon iconoApagado = new ImageIcon(focoApagado.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+	
+	ImageIcon focoEncendido = new ImageIcon("src/recursos/encendido.png");
+	Icon iconoEncendido = new ImageIcon(focoEncendido.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT));
+	
+	boolean estados [] = {false, false , false, false, false, false, false, false};
+	int valores [] = {128,64, 32, 16, 8, 4,2,1}; 
+	int suma= 0;
 	
 	public Contenido()
 	{
-		setBackground(new Color(125,119,104));
+		setBackground(new Color(220,220,220));
 		setLayout(new FlowLayout(FlowLayout.CENTER));
 		
 		inicializarBotones(botones);
+		decimalLabel.setForeground(Color.BLACK);
 		//setLayout(new BorderLayout());
 		
 	}
@@ -35,10 +48,47 @@ public class Contenido extends JPanel{
 			arrayBotones[i].setIcon(icono);
 			arrayBotones[i].setBackground(Color.white);
 			add(arrayBotones[i]);
-			arrayBotones[i].addActionListener(new Decimal());
+			arrayBotones[i].addActionListener(this);
 		}
 		
 		add(decimalLabel);
 	}
 
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		int botonId =Integer.parseInt(((JButton) e.getSource()).getName()); 
+		JButton botonPulsado = (JButton) e.getSource();
+//		System.out.println(botonId);
+		
+		if(estados[botonId] ==false)
+		{
+			botonPulsado.setIcon(iconoEncendido);
+			estados[botonId] = true;
+				
+			
+		}else {
+			botonPulsado.setIcon(iconoApagado);
+			estados[botonId] = false;
+			
+		}
+		
+		
+		suma = calcularBinario(estados);
+		
+		decimalLabel.setText("Decimal: " + suma);
+	}
+	
+	public int calcularBinario(boolean[] binario) {
+		int resultado = 0;
+
+		for(int i= 0; i<binario.length; i++) {
+			if(binario[i] == true) {
+				resultado += valores[i];
+			}
+		}
+		
+		return resultado;
+	}
+
+	}
